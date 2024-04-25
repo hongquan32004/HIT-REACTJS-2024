4cf4fe2b943ffe2d71d6a2724baeada967036c45
hongquan32004 <117972211+hongquan32004@users.noreply.github.com>
2024-04-25 12:33:59 +0700

    update day6

--- HomeWork/src/App.jsx
+++ HomeWork/src/App.jsx
@@ -4,16 +4,42 @@ import viteLogo from '/vite.svg'
 import './App.scss'
 import './scss/_index.scss'
 import Todoapp from './component/Todoapp/Todoapp'
-import Login from './component/Login/Login'
+import Login from './pages/Login/Login'
+import AboutUs from './pages/AboutUs/AboutUs'
+import Home from './pages/Home/Home'
+import Contact from './pages/Contact/Contact'
+import { Route, Routes, Link, NavLink } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
+  const login = localStorage.getItem('login');
+  console.log(login)
+  const isactiveClassName = ({ isActive }) => {
+    return isActive ? 'active' : '';
+  };
 
   return (
     <>
+      {/* <h1>{login ? "chua login" : "da login"}</h1>
       <div className='back-ground'>
+
         <Login></Login>
-      </div>
+      </div> */}
+      <nav>
+        <ul>
+          <li><NavLink to="/" className={isactiveClassName}>Home</NavLink></li>
+          <li><NavLink to="/about-us" className={isactiveClassName}>About us</NavLink></li>
+          <li><NavLink to="/contact" className={isactiveClassName}>Contact</NavLink></li>
+        </ul>
+        <br /><br />
+
+      </nav>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about-us' element={<AboutUs />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='*' element={<h1>Not found</h1>} />
+      </Routes>
     </>
   )
 }
--- HomeWork/src/pages/Login/Login.jsx
+++ HomeWork/src/pages/Login/Login.jsx
@@ -2,7 +2,7 @@ import { memo } from "react"
 import './Login.scss'
 import { useFormik, Formik, Field, Form } from 'formik'
 import * as Yup from 'yup'
-
+import axios from 'axios'
 const Login = () => {
     return (
         <div className="login">
@@ -15,25 +15,36 @@ const Login = () => {
                     username: Yup.string()
                         .required("Bạn không được bỏ trống.")
                         .min(5, "Bạn phải nhập ít nhất 5 kí tự")
-                        .matches(/^[A-Z][a-z]*(\s[A-Z][a-z]*)*$/, "Tên đăng nhập không hợp lệ."),
+                    // .matches(/^[A-Z][a-z]*(\s[A-Z][a-z]*)*$/, "Tên đăng nhập không hợp lệ.")
+                    ,
                     password: Yup.string()
                         .required("Bạn không được bỏ trống.")
-                        .min(8, "Bạn phải nhập tối thiểu 8 kí tự.")
-                        .matches(/^(?=.*[A-Z])(?=.*[a-z].*[a-z])(?=.*\d)(?=.*[!@#$%^&*()_+])[A-Za-z\d!@#$%^&*()_+]/,
-                            'Mật khẩu không hợp lệ.')
+                        .min(4, "Bạn phải nhập tối thiểu 8 kí tự.")
+                    // .matches(/^(?=.*[A-Z])(?=.*[a-z].*[a-z])(?=.*\d)(?=.*[!@#$%^&*()_+])[A-Za-z\d!@#$%^&*()_+]/,
+                    //     'Mật khẩu không hợp lệ.')
                 })}
-                onSubmit={(values) => {
-                    console.log(values)
+                onSubmit={async (values) => {
+                    try {
+                        console.log("Post")
+                        const result = await axios.post("https://dummyjson.com/auth/login", values);
+                        console.log(result)
+                        localStorage.setItem("login", result.token);
+                        sessionStorage.setItem("login", result.token);
+                    }
+                    catch (error) {
+                        console.log(error)
+                    }
+
                 }}
 
             >
                 {({ errors, touched }) => (
                     <Form className="form">
                         <Field className="input" type="text" name="username" id="username" placeholder="username" />
-                        {errors.username && touched.username && (<p style={{ color: 'red', fontSize: '15px' }}>{errors.username}</p>)}
+                        {errors.username && touched.username && (<p className="p1" style={{ color: 'red', fontSize: '15px' }}>{errors.username}</p>)}
                         <br />
                         <Field className="input" type="password" name="password" id="password" placeholder="password" />
-                        {errors.password && touched.password && (<p style={{ color: 'red', fontSize: '15px' }}>{errors.password}</p>)}
+                        {errors.password && touched.password && (<p className="p2" style={{ color: 'red', fontSize: '15px' }}>{errors.password}</p>)}
                         <br />
                         <Field className="submit" type="submit" name="submit" id="submit" value='Submit form' />
                         <div className="card-a-login">
@@ -48,4 +59,4 @@ const Login = () => {
         </div >
     )
 }
-export default memo(Login);
+export default Login;
